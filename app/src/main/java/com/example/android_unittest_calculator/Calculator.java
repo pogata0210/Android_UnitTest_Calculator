7dd135dcf430459380098a81e93f7157600c757a
Pablo Ogata <pabloogata@gmail.com>
2019-02-08 17:28:13 -0500

    addSymbol

--- app/src/main/java/com/example/android_unittest_calculator/Calculator.java
+++ app/src/main/java/com/example/android_unittest_calculator/Calculator.java
@@ -1,4 +1,17 @@
 package com.example.android_unittest_calculator;
 
 public class Calculator {
+    private static final String TAG = "Calculator";
+
+    String enteredString;
+    String lastEnteredString;
+
+    public Calculator() {
+        this.enteredString = "";
+        this.lastEnteredString = "";
+    }
+
+    public String addSymbol(String symbol) {
+        return enteredString;
+    }
 }
